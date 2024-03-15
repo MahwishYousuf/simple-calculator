331f0b9ace09ac587ba312c5e5e4e2a3690cc51e
MahwishYousuf <161046596+MahwishYousuf@users.noreply.github.com>
2024-03-16 01:20:10 +0500

    sir zia assignment linkedIn
    simple calculator by mahwish saleem 
    governor house AI Initiative student 
    tuesday 2-5

---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        message: "Enter first number",
+        type: "number",
+        name: "firstNumber",
+    },
+    {
+        message: "Enter first number",
+        type: "number",
+        name: "secondNumber",
+    },
+    { message: "Select one of the operators to perform action",
+        type: "list",
+        name: "operators",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+//conditional Statement 
+if (answer.operators === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operators === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operators === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operators === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("Please select valid operators");
+}
